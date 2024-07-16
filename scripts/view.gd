705796977befa90f0ad9c8326d38132756753395
IoriBranford <ioribranford@gmail.com>
2024-07-16 16:18:09 -0700

    Support disabling camera rotation/zoom input.

--- scripts/view.gd
+++ scripts/view.gd
@@ -7,11 +7,13 @@ extends Node3D
 @export var target_offset: Vector3
 
 @export_group("Zoom")
+@export var zoom_input_enabled = true
 @export var zoom_minimum = 16
 @export var zoom_maximum = 4
 @export var zoom_speed = 10
 
 @export_group("Rotation")
+@export var rotation_input_enabled = true
 @export var rotation_speed = 120
 
 var camera_rotation:Vector3
@@ -47,10 +49,12 @@ func handle_input(delta):
 	input.y = Input.get_axis("camera_left", "camera_right")
 	input.x = Input.get_axis("camera_up", "camera_down")
 	
-	camera_rotation += input.limit_length(1.0) * rotation_speed * delta
-	camera_rotation.x = clamp(camera_rotation.x, -80, -10)
+	if rotation_input_enabled:
+		camera_rotation += input.limit_length(1.0) * rotation_speed * delta
+		camera_rotation.x = clamp(camera_rotation.x, -80, -10)
 	
 	# Zooming
 	
-	zoom += Input.get_axis("zoom_in", "zoom_out") * zoom_speed * delta
-	zoom = clamp(zoom, zoom_maximum, zoom_minimum)
+	if zoom_input_enabled:
+		zoom += Input.get_axis("zoom_in", "zoom_out") * zoom_speed * delta
+		zoom = clamp(zoom, zoom_maximum, zoom_minimum)
