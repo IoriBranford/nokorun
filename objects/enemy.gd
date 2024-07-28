6e62a6488ea9940329d285ccb22ec98eed49e88d
IoriBranford <ioribranford@gmail.com>
2024-07-28 15:54:11 -0700

    Code for enemy initial animation and death.

--- objects/enemy.gd
+++ objects/enemy.gd
@@ -1,11 +1,20 @@
+class_name Enemy
 extends Node3D
 
+@onready var animationPlayer = $model/AnimationPlayer
+
+var alive = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	animationPlayer.play("gun_idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	pass
+
+func try_kill():
+	if alive:
+		alive = false
+		animationPlayer.play("dead2")
+		return true
