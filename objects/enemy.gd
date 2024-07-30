b794cc99278db1044766aa9dc22578f15e9671ed
IoriBranford <ioribranford@gmail.com>
2024-07-29 20:59:18 -0700

    Enemy aims at player while alive.

--- objects/enemy.gd
+++ objects/enemy.gd
@@ -15,13 +15,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if not alive:
+	if alive:
+		if player:
+			look_at(player.global_position, Vector3.UP, true)
+	else:
 		if velocity.length_squared() >= 1:
 			velocity += Vector3.FORWARD * KNOCKBACK_FORCE * delta
 			self.move_and_slide()
 
 func try_kill():
 	if alive:
+		look_at(global_position + Vector3.FORWARD, Vector3.UP, true)
 		set_collision_layer_value(CollisionLayer.ENEMY_BODY, false)
 		$RayCast3D.enabled = false
 		$RayCast3D.visible = false
